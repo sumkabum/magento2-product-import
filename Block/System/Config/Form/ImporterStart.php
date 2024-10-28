53266985c6fab70926e15c6dffa9a688e44da181
raulikesvatera <raulikesvatera@gmail.com>
2024-10-28 16:34:40 +0200

    admin config start import button without stop
    (cherry picked from commit 2bf741a7a971ed28f3bc26e13043c96b8f8ba549)

---
+++ Block/System/Config/Form/ImporterStart.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Sumkabum\Magento2ProductImport\Block\System\Config\Form;
+
+use Sumkabum\Magento2ProductImport\Service\Scheduler;
+
+class ImporterStart extends \Magento\Config\Block\System\Config\Form\Field
+{
+    public const IMPORTER_JOB_CODE = null;
+
+    protected function _renderValue(\Magento\Framework\Data\Form\Element\AbstractElement $element)
+    {
+        /** @var \Magento\Framework\View\Element\Template $block */
+        $block = $this->getLayout()->createBlock(static::class);
+        $block->setTemplate('Sumkabum_Magento2ProductImport::config/importer_start.phtml');
+        return $block->toHtml();
+    }
+}
---
+++ Controller/Adminhtml/Config/ImportStart.php
@@ -0,0 +1,44 @@
+<?php
+namespace Sumkabum\Magento2ProductImport\Controller\Adminhtml\Config;
+
+use Exception;
+use Magento\Backend\App\AbstractAction;
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\View\LayoutInterface;
+
+class ImportStart extends AbstractAction
+{
+    /**
+     * @var LayoutInterface
+     */
+    private $layout;
+
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        LayoutInterface $layout
+    ) {
+        parent::__construct($context);
+        $this->layout = $layout;
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function execute(): \Magento\Framework\Controller\Result\Json
+    {
+        /** @var \Magento\Framework\Controller\Result\Json $result */
+        $result = $this->resultFactory->create(ResultFactory::TYPE_JSON);
+
+        /** @var \Magento\Framework\View\Element\Template $block */
+        $block = $this->layout->createBlock(\Magento\Framework\View\Element\Template::class);
+        $block->setTemplate('Sumkabum_Magento2ProductImport::config/importer_start_ajax_content.phtml');
+        $block->setData('job_code', $this->getRequest()->getParam('job_code'));
+
+        $data = [
+            'html' => $block->toHtml()
+        ];
+        $result->setData($data);
+
+        return $result;
+    }
+}
